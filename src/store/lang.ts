c6499850a32614be7fed29108e7b1fc921bbd207
陈云 <HighWindCircles@163.com>
2023-08-18 14:58:35 +0800

    .

--- src/store/lang.ts
+++
@@ -1,8 +0,0 @@
-import { defineStore } from 'pinia'
-import {ref} from 'vue';
-
-export const useLangStore = defineStore('lang', () => {
-    let lang = ref(localStorage.getItem('lang_sale_ticket_website') || 'en_GB')
-    console.log(1, lang.value)
-    return { lang }
-})
